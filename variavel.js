a9867866a90a1b25d4c38d0e6950652ddaa7dc6a
Gabrielende5 <gabrielende53@gmail.com>
2023-03-30 17:10:19 -0300

    tipos de variavel

---
+++ variavel.js
@@ -0,0 +1,10 @@
+var variavel = 12 ;
+const nome = "Nome de alguem";
+// a variavel do tipo let só existe dentro da função somar 
+function somar(numero){
+    let futuro = 10;
+    console.log("no futuro você terá ",futuro+ numero, "anos")
+
+}
+// somar(21);
+// variávels -> var (variável), const (constante), let (estático)
